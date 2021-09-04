374bc0d34a45e1ce20417b30f2607ec5b07526ca
ronah289 <ronald.kibet@student.moringaschool.com>
2021-09-04 11:56:47 -0230

    add input variable holders

---
+++ js/index.js
@@ -0,0 +1,8 @@
+function findName(){
+    // get input
+    var date = document.getElementById("dateOfBirth").value;
+    var month = document.getElementById("birthMonth").value;
+    var year = document.getElementById("birthYear").value;
+    //var gender = document.getElementsName("gender");
+    alert(date+"......"+month+"......"+year);
+}
