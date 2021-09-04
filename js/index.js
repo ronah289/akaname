8233737c05143154875315ce23e9cb6e84f77228
ronah289 <ronald.kibet@student.moringaschool.com>
2021-09-04 15:48:59 -0230

    create alert for values

--- js/index.js
+++ js/index.js
@@ -8,30 +8,28 @@ function findName(){
     var month = document.getElementById("birthMonth").value;
     var year = document.getElementById("birthYear").value;
     var gender = document.getElementsByName("gender");
-    var g = gender[0].checked? "female":"male";
+    var genderValue = gender[0].checked? "female":"male";
+    var genderHold = genderValue;
     // error handler
     function error(){
-        date = "check your date";
-        month = "check your month";
-        year = "check your year";
+        alert("check inputs!!!!");
+
     }
     if(isNaN(year)|| isNaN(month)||isNaN(date)){
         error();
     }
-    else if(year%4 ==0 || year % 400 == 0){
-        Dates = [...Dates][1]=29;
-        if(date >= 1 && date<=(Dates[month-1])){
-            date = date;
-            approvedDay = new Array(year,month,Day);
-            alert(approvedDay);
-
-        }
-    }
     else{
-        Dates = [...Dates];
-        if(date >= 1 && date<=(Dates[month-1])){
-            date = date;
-            approvedDay = new Array(year,month,date);
+        year = (year.toString().length == 4)?year:error();
+        month = (month >= 1 && month <=12)?month:error();
+        date = (date >=1 && date <= Dates[month-1])?date:error();
+        if(year%4 ==0 || year % 400 == 0){
+            Dates = [...Dates][1]=29;
+            approvedDay = new Array(year,month,date,genderHold);
+            alert(approvedDay);
+        }
+        else{
+            Dates = [...Dates];
+            approvedDay = new Array(year,month,date,genderHold);
             alert(approvedDay);
         }
     }
